b58738ebfa054932a0a83ae77daf80f5d0f50d3b
timecapspro <git@timecaps.pro>
2025-12-29 20:19:26 +0300

    Add Kinopoisk API key to backend params

--- backend/config/params.php
+++ backend/config/params.php
@@ -3,4 +3,5 @@ return [
     'adminEmail' => 'admin@example.com',
     'jwtSecret' => getenv('JWT_SECRET') ?: null,
     'jwtTtl' => getenv('JWT_TTL') ? (int)getenv('JWT_TTL') : null,
+    'kinopoiskApiKey' => 'c7c43696-f623-415a-bbad-019e665f448b',
 ];
